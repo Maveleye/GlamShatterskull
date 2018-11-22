a63f13208f99b545938286d6591d379fdef46f6e
Maveleye <stephen.sartin@gmail.com>
2018-11-21 19:12:08 -0600

    Start of list script

---
+++ BirthdayList.py
@@ -0,0 +1,22 @@
+#-----------------------------------
+# Libraries and References
+#-----------------------------------
+
+
+#-----------------------------------
+# [Required]	Script Information
+#-----------------------------------
+ScriptName = "BirthdayList"
+Website = "https://www.twitch.tv/maveleye"
+Creator = "Maveleye"
+Version = 0.1
+Description = "Query current month from a source to display all birthdays on stream."
+
+#-----------------------------------
+# Set Variables
+#-----------------------------------
+
+
+#-----------------------------------
+# TODO build script
+#-----------------------------------
